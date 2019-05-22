36267617a1db7f397a797d3238470e9f5e15fde2
Jhonny Martinez <jmartinez@typeiqs.com>
2019-05-22 09:11:08 -0400

    Translation features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'Ephasic';
+  
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('es');
+  }
+  
+  title = '3phasic';
+
+  useLanguage(language: string) {
+    this.translate.use(language);
+  }
+
 }
